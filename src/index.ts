7da86fc076e80e37fb2ab4633f0e96ed553c4db9
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2022-10-02 18:40:18 +0200

    restructure

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-import * as standardJWT from "./standard/jwt";
-import * as standardParser from "./standard/parser";
-import * as standardVerifier from "./standard/verifier";
-import * as standardIssuer from "./standard/issuer";
-export * as types from "./types";
+export * as JWT from "./jwt/index"
 export * as jose from "jose";
-/**
- * Functional classes for a standard JWT Token
- */
-export const standardJWTToken = {
-    Token: standardJWT.JWTToken,
-    Header: standardJWT.JWTHeader,
-    Payload: standardJWT.JWTPayload,
-    Parser: standardParser.JWTTokenParser,
-    Verifier: standardVerifier.JWTTokenVerifier,
-    Issuer: standardIssuer.JWTTokenIssuer,
-};
 
 async function main(): Promise<void> {}
 
