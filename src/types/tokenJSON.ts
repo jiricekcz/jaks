af7cb1f03b2fd7826c79e0c12abde500e02cf05f
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-05-05 11:19:15 +0200

    json types

--- src/types/tokenJSON.ts
+++ src/types/tokenJSON.ts
@@ -1,6 +1,8 @@
 // Types for JSON versions of the token
 
+import { VersionString } from "./jaks";
 import { TokenConfiguration } from "./tokenConfig";
+import { Base64Url, PropFromHas } from "./util";
 
 /**
  * A JSON representation of a token.
@@ -24,7 +26,15 @@ export interface TokenJSON<Configuration extends TokenConfiguration> {
  * A JSON representation of a token header.
  */
 export interface TokenHeaderJSON<Configuration extends TokenConfiguration> {
-
+    /**
+     * Algorithm used to sign the token.
+     */
+    "alg": Configuration["header"]["alg"];
+    
+    /**
+     * JAKS version string.
+     */
+    "jaks": VersionString;
 }
 
 /**
@@ -32,11 +42,68 @@ export interface TokenHeaderJSON<Configuration extends TokenConfiguration> {
  */
 export interface TokenPayloadJSON<Configuration extends TokenConfiguration> {
 
+    /**
+     * Issuer of the token.
+     * @see https://www.rfc-editor.org/rfc/rfc7519.html#section-4.1.1
+     */
+    "iss": Configuration["payload"]["iss"];
+
+    /**
+     * Subject of the token.
+     * @see https://www.rfc-editor.org/rfc/rfc7519.html#section-4.1.2
+     */
+    "sub": Configuration["payload"]["sub"];
+
+    /**
+     * Audience of the token.
+     * @see https://www.rfc-editor.org/rfc/rfc7519.html#section-4.1.3
+     */
+    "aud": Configuration["payload"]["aud"];
+
+    /**
+     * Expiration time of the token.
+     * Provided in seconds since the Unix epoch.
+     * @see https://www.rfc-editor.org/rfc/rfc7519.html#section-4.1.4
+     */
+    "exp": number;
+
+    /**
+     * Not before time of the token.
+     * Provided in seconds since the Unix epoch.
+     * @see https://www.rfc-editor.org/rfc/rfc7519.html#section-4.1.5
+     */
+    "nbf": PropFromHas<Configuration["payload"]["hasNBF"], number>;
+
+    /**
+     * Issued at time of the token.
+     * Provided in seconds since the Unix epoch.
+     * @see https://www.rfc-editor.org/rfc/rfc7519.html#section-4.1.6
+     */
+    "iat": PropFromHas<Configuration["payload"]["hasIAT"], number>;
+
+    /**
+     * JWT ID of the token.
+     * @see https://www.rfc-editor.org/rfc/rfc7519.html#section-4.1.7
+     */
+    "jti": Configuration["payload"]["jti"];
+
+    /**
+     * JAKS plugins of the token and their data.
+     */
+    "plg": TokenPluginsJSON<Configuration>;
 }
 
 /**
  * A JSON representation of a token signature.
  */
 export interface TokenSignatureJSON<Configuration extends TokenConfiguration> {
+    /**
+     * The signature of the token.
+     */
+    signature: Base64Url;
+}
+
+
+export interface TokenPluginsJSON<Configuration extends TokenConfiguration> {
 
 }
