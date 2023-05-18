956e656799606d23b0d1fc4fdf21bf22d92080e0
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-05-18 13:20:02 +0200

    parssing tokens to token manipulation interfaces

--- src/types/tokenManipulation.ts
+++ src/types/tokenManipulation.ts
@@ -1,6 +1,6 @@
 // Interfaces for classes that manipulate tokens
 
-import { Token } from "./token";
+import { Token as IToken } from "./token";
 import { TokenConfiguration } from "./tokenConfig";
 import { TokenString } from "./util";
 
@@ -8,12 +8,12 @@ import { TokenString } from "./util";
  * A class that implements the parser interface can parse a string into a token.  
  * Configuration of the parser is provided by the generic type. You should provide your token configuration type.
  */
-export interface Parser<Configuration extends TokenConfiguration> {
+export interface Parser<Configuration extends TokenConfiguration, Token extends IToken<Configuration>> {
     /**
      * The parse function parses a token string into a token object.
      * @param tokenString Token string to parse.
      */
-    parseToken(tokenString: TokenString): Promise<Token<Configuration>>;
+    parseToken(tokenString: TokenString): Promise<Token>;
 }
 
 /**
@@ -21,12 +21,12 @@ export interface Parser<Configuration extends TokenConfiguration> {
  * Configuration of the verifier is provided by the generic type. You should provide your token configuration type.
  * A verifier needs to be able to parse the token, so it extends the parser interface.
  */
-export interface Verifier<Configuration extends TokenConfiguration> extends Parser<Configuration> {
+export interface Verifier<Configuration extends TokenConfiguration, Token extends IToken<Configuration>> extends Parser<Configuration, Token> {
     /**
      * The verifyToken function verifies the signature of a token.
      * @param token Token to verify.
      */
-    verifyToken(token: Token<Configuration>): Promise<boolean>;
+    verifyToken(token: Token): Promise<boolean>;
 
     /**
      * Parses a token string to a token and then verifies the signature of the token.
@@ -34,7 +34,7 @@ export interface Verifier<Configuration extends TokenConfiguration> extends Pars
      * If the verification fails, returns null.
      * @param tokenString
      */
-    parseThenVerifyToken(tokenString: TokenString): Promise<Token<Configuration> | null>;
+    parseThenVerifyToken(tokenString: TokenString): Promise<Token| null>;
 }
 
 /**
@@ -43,10 +43,10 @@ export interface Verifier<Configuration extends TokenConfiguration> extends Pars
  * The issuer also needs another generic type for the options to issue a token. This type isn't a part of the configuration in order to retain the abilty to make multiple different token issuers for the same token.
  * An issuer needs to be able to parse the token and also should be able to verify the token, so it extends the parser and verifier interfaces.
  */
-export interface Issuer<Configuration extends TokenConfiguration, IssueOptions> extends Verifier<Configuration> {
+export interface Issuer<Configuration extends TokenConfiguration, Token extends IToken<Configuration>, IssueOptions> extends Verifier<Configuration, Token> {
     /**
      * Issues a signed token.
      * @param options Options to issue a token. Determined by the generic type.
      */
-    issueToken(options: IssueOptions): Promise<Token<Configuration>>;
+    issueToken(options: IssueOptions): Promise<Token>;
 }
